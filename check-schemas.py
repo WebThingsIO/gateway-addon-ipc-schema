3c551a21e5902b4e9b08c538162c024e0260349f
Michael Stegeman <michael@stegeman.me>
2019-11-19 09:03:55 -0900

    Fix ID of notifier-unload-response.
    Fixes mozilla-iot/gateway#2298

--- check-schemas.py
+++ check-schemas.py
@@ -12,6 +12,7 @@ _BASE = os.path.realpath(os.path.dirname(__file__))
 def main():
     files = [os.path.join(_BASE, 'schema.json')]
     files.extend(glob.glob(os.path.join(_BASE, 'messages', '*.json')))
+    ids = {}
 
     for fname in files:
         try:
@@ -28,6 +29,18 @@ def main():
             print('Error in schema:', fname)
             print(e)
             sys.exit(1)
+
+        if not os.path.basename(fname) == 'schema.json' and \
+                not os.path.basename(fname) == 'definitions.json':
+            _id = schema['properties']['messageType']['const']
+
+            if _id in ids:
+                print('Duplicate id: {}'.format(_id))
+                print('  {}'.format(ids[_id]))
+                print('  {}'.format(fname))
+                sys.exit(1)
+
+            ids[_id] = fname
 
 
 if __name__ == '__main__':
